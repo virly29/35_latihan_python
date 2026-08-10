69d9f1b41b1ee7c85bab605df9d2e618909e4e27
29_VIRLY <vapriliawardani@gmail.com>
2026-08-10 13:47:36 +0700

    Add files via upload
    Menambahkan perulangan while loop ke program bilangan ganjil genap

---
+++ Virly_35.py
@@ -0,0 +1,12 @@
+angka = int(input("Masukkan angka: "))
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap.")
+else:
+    print(f"{angka} adalah bilangan ganjil.")
+while True:
+    angka = int(input("Masukkan angka: "))
+    if angka % 2 == 0:
+        print(f"{angka} adalah bilangan genap.")
+    else:
+        print(f"{angka} adalah bilangan ganjil.")
+        
